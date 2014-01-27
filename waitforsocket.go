e242e8cd3e4ec93d4a6e8185341406c7d8c7c84c
Dustin Sallings <dustin@spy.net>
2014-01-27 10:36:01 -0800

    Rewrote in go, added multi-destination feature

---
+++ waitforsocket.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+var (
+	timeout  = flag.Duration("timeout", 5*time.Second, "connection/retry timeout")
+	required = flag.Int("required", 0, "How many connections required (0 == all)")
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr,
+			"Usage: waitforsocket [opts] host:service|port [...]\n")
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+	}
+}
+
+func try(addr string) error {
+	c, err := net.DialTimeout("tcp", addr, *timeout)
+	if err == nil {
+		c.Close()
+	}
+	return err
+}
+
+type res struct {
+	addr string
+	t    time.Time
+}
+
+func wait(addr string, ch chan res) {
+	ticker := time.Tick(*timeout)
+	for {
+		if err := try(addr); err == nil {
+			ch <- res{addr, time.Now()}
+			return
+		} else {
+			log.Printf("%v", err)
+			<-ticker
+		}
+	}
+
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(64)
+	}
+
+	if *required == 0 {
+		*required = flag.NArg()
+	}
+
+	ch := make(chan res)
+
+	for _, hp := range flag.Args() {
+		go wait(hp, ch)
+	}
+
+	responses := 0
+	for responses < *required {
+		r := <-ch
+		responses++
+		log.Printf("Connected to %v", r.addr)
+	}
+}
--- waitforsocket.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2005  Dustin Sallings <dustin@spy.net>
- */
-
-enum returnvalues { ERR_DNS=-3, ERR_TIMEOUT=-2, ERR_ERRNO=-1, RV_SUCCESS=0 };
-
-enum returnvalues attemptConnection(char *, char *);
