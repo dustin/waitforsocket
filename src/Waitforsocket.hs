4fe610905c8edd3f559459c3234ef36969172958
Dustin Sallings <dustin@spy.net>
2022-07-03 20:51:23 -1000

    Simplify parsing a bit

--- src/Waitforsocket.hs
+++ src/Waitforsocket.hs
@@ -18,7 +18,6 @@ import           Control.Concurrent.Async (Async, waitAny)
 import           Control.Monad            (guard, unless)
 import qualified Data.Attoparsec.Text     as A
 import           Data.Foldable            (fold)
-import           Data.String              (fromString)
 import qualified Data.Text                as T
 import           Data.Time.Clock          (NominalDiffTime, diffUTCTime, getCurrentTime)
 import           Network.Socket           (HostName, ServiceName)
@@ -31,31 +30,31 @@ instance Show Target where
   show (TCP s p) = fold ["tcp@", s, ":", p]
   show (HTTP s)  = fold ["web@", s]
 
-hostnameParser :: A.Parser T.Text
-hostnameParser = T.intercalate "." <$> hpart `A.sepBy1` A.char '.'
+hostnameParser :: A.Parser HostName
+hostnameParser = T.unpack . T.intercalate "." <$> hpart `A.sepBy1` A.char '.'
   where hpart :: A.Parser T.Text
         hpart = do
           t <- A.takeWhile1 (A.inClass "A-z0-9-")
           guard $ '-' `notElem` [T.head t, T.last t]
           pure t
 
-hostPortParser :: A.Parser (T.Text, T.Text)
-hostPortParser = (,) <$> (hostnameParser <* ":") <*> A.takeWhile (A.inClass "A-z0-9")
+hostPortParser :: A.Parser (HostName, ServiceName)
+hostPortParser = (,) <$> (hostnameParser <* ":") <*> (T.unpack <$> A.takeWhile (A.inClass "A-z0-9"))
 
-newtype URL = URL T.Text deriving (Show)
+newtype URL = URL { unURL :: String } deriving (Show)
 
 urlParser :: A.Parser URL
 urlParser = do
   prot <- A.string "http://" <|> A.string "https://"
   host <- hostnameParser
   rest <- A.takeText
-  pure $ URL $ fold [prot, host, rest]
+  pure . URL $ fold [T.unpack prot, host, T.unpack rest]
 
 parseTarget :: String -> Either String Target
-parseTarget = A.parseOnly (http <|> tcp) . fromString
+parseTarget = A.parseOnly (http <|> tcp) . T.pack
   where
-    http = urlParser >>= \(URL u) -> pure $ HTTP (T.unpack u)
-    tcp = hostPortParser >>= \(h,p) -> pure $ TCP (T.unpack h) (T.unpack p)
+    http = HTTP . unURL <$> urlParser
+    tcp = uncurry TCP <$> hostPortParser
 
 while :: Natural -> IO (Maybe Bool) -> IO ()
 while d f = f >>= \v -> unless (v == Just True) $ threadDelay (fromIntegral d) *> while d f
